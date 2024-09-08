317e227b59cbce1a28b79ab320a70f8056ff5675
ma.martynov <ma.martynov@tinkoff.ru>
2024-09-08 21:41:03 +0300

    Монго работает но приложение к нему не умеетт подключаться.

---
+++ mongo-sharding/01_init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x
+
+###
+# Инициализируем бд
+###
+
+docker compose exec configSrv sh -c "mongosh --port 27017 < /scripts/init-configserver.js"
+
+docker compose exec shard1 sh -c "mongosh --port 27018 < /scripts/init-shard01.js"
+docker compose exec shard2 sh -c "mongosh --port 27019 < /scripts/init-shard02.js"
+
+docker compose exec mongos_router sh -c "mongosh --port 27020 < /scripts/init-mongos-router.js"
